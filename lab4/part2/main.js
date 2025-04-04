512be021bad32c643dfc275fc4874184911ba2cc
Justin <justin.buss@dcmail.ca>
2025-04-04 14:49:27 -0400

    Majority content added
    Added declaring image array
    Added alt text of images
    TODO - looping through images
    Added Darkening/Lightening button

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -1,3 +1,14 @@
+// <!--
+// Name: Justin Buss
+// File: index.html
+// Date Created: April 4, 2025
+// Date last modified: April 4, 2025
+// Description: A generator to create a silly story.
+// -->
+
+
+//Helpful -> https://www.w3schools.com/jsref/event_onclick.asp
+ 
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
@@ -5,14 +16,35 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg'];
 
 /* Declaring the alternative text for each image file */
-
+const altText = {'pic1.jpg':'Closeup of a human eye',
+                 'pic2.jpg':'Photo of a wavy stone painting',
+                 'pic3.jpg':'Photo of purple flowers and white flowers',
+                 'pic4.jpg':'photo of ancient egyptian drawings'
+}
 /* Looping through images */
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
+for(image of imageArray){
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', imageArray);
+    newImage.setAttribute('alt', altText);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener
+}
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', toggleDark)
+
+function toggleDark(){
+    if(btn.getAttribute('class')=='dark'){
+        btn.setAttribute('class','light')
+        btn.textContent('Lighten')
+        overlay.computedStyleMap.backgroundColor = "rgb(0 0 0 / 50%)"
+    }else{
+        btn.setAttribute('class', 'dark')
+        btn.textContent('Darken')
+        overlay.computedStyleMap.backgroundColor = "rgb(0 0 0 / 0%)"
+    }
+}

41d26b034832b499c93a0bb077882748bd3641c8
Justin <justin.buss@dcmail.ca>
2025-04-04 15:46:11 -0400

    Final pastes
    Final version with all completed parts.
    The balls bounce around and change color when collided with.

--- lab4/part3/main.js
+++ lab4/part3/main.js
@@ -67,11 +67,25 @@ class Ball {
     this.y += this.velY;
   }
 
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+  
 
 }
 const balls = [];
 
-while (balls.length < 25) {
+while (balls.length < 100) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
@@ -94,6 +108,7 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);

1fb6a467bca321a4402fd85a8650f779caca15b3
Justin <justin.buss@dcmail.ca>
2025-04-04 17:21:23 -0400

    Finished assignment & changed file name
    Added EvilCircle and Shape classes
    Added killer
    Added ball counter

--- lab4/part4/main.js
+++ lab4/part4/main.js
@@ -9,6 +9,7 @@
 // set up canvas
 
 const canvas = document.querySelector("canvas");
+const counter = document.querySelector("h2");
 const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
@@ -25,15 +26,20 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
@@ -66,7 +72,8 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      //if (!(this === ball)) { from La4 Part 3, now add the condition the ball has not hit the Evil Circle
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -78,9 +85,70 @@ class Ball {
     }
   }
 }
+class EvilCircle extends Shape{
+  constructor(x, y, velX, velY) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+  draw() {
+    ctx.beginPath();
+    ctx.strokeStyle = this.color;
+    ctx.lineWidth = 3;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  checkBounds() {
+    if (this.x + this.size >= width) {
+      this.x -= 20;
+    }
 
+    if (this.x - this.size <= 0) {
+      this.x += 20;
+    }
+
+    if (this.y + this.size >= height) {
+      this.y -= 20;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.y += 20;
+    }
+  }
+  collisionDetect() {
+    for (const ball of balls) {
+      //if (!(this === ball)) { from La4 Part 3, now add the condition the ball has not hit the Evil Circle
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+          count--;
+        }
+      }
+    }
+  }
+
+}
 const balls = [];
-
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
@@ -96,17 +164,24 @@ while (balls.length < 25) {
 
   balls.push(ball);
 }
+const killer = new EvilCircle(100, 100); 
+let count = balls.length;
 
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
 
   for (const ball of balls) {
-    ball.draw();
-    ball.update();
-    ball.collisionDetect();
+    if(ball.exists){
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
   }
-
+  killer.draw();
+  killer.checkBounds();
+  killer.collisionDetect();
+  counter.textContent = "Count: " + count;
   requestAnimationFrame(loop);
 }
 
